1442d49472631b14925f1bd893c19beebf53ea74
U027606 <arantzazu.otal.ext@rsi.cajarural.es>
2023-11-11 10:57:24 +0100

    Cash register

--- app/domain/basket_domain.rb
+++ app/domain/basket_domain.rb
@@ -1,12 +1,13 @@
 module BasketDomain
   module_function
 
-  def calculate_total_prize(product_basket)
-    total_prize = product_basket.basket.total_prize + (product_basket.amount * product_basket.product.prize)
-    product_basket.basket.update(total_prize:total_prize)
+  def calculate_total_prize(product_basket, amount)
+    product_basket.update(amount:product_basket.amount+amount) if amount
+    total_prize = product_basket.basket.total_prize + (amount * product_basket.product.prize) if amount
+    product_basket.basket.update(total_prize:total_prize) if total_prize
   end
 
   def amount_prize(product_basket)
-    product_basket.amount * product_basket.product.prize
+    product_basket.amount * product_basket.product.prize if product_basket.amount
   end
 end
