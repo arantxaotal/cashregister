242ddf1ead94f456d9b03a6ffcecae2fe405a1fe
U027606 <arantzazu.otal.ext@rsi.cajarural.es>
2023-11-12 11:48:03 +0100

    Cash register testing

--- app/controllers/product_baskets_controller.rb
+++ app/controllers/product_baskets_controller.rb
@@ -1,6 +1,7 @@
 class ProductBasketsController < ApplicationController
 
   def product_basket_delete
+
     @product_basket = ProductBasket.find_by_id(params[:product_basket_id])
     @product_basket.basket.update(total_prize:BasketDomain.remove_product_amount(@product_basket))
     @product_basket.destroy
