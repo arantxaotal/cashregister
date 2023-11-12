7598ccf6171e7cf6e32639a7c5fedf0074f991b1
U027606 <arantzazu.otal.ext@rsi.cajarural.es>
2023-11-12 11:49:41 +0100

    Cash register testing

--- app/controllers/basket_controller.rb
+++ app/controllers/basket_controller.rb
@@ -2,8 +2,10 @@ class BasketController < ApplicationController
 
   def edit
     @basket = Basket.find(params[:id])
-    product_basket = ProductBasket.find_or_create_by(basket_id:@basket.id,product_id:params[:product_id])
-    BasketDomain.calculate_total_prize(product_basket,params[:amount].to_i)
+    if params[:amount].to_i > 0
+      product_basket = ProductBasket.find_or_create_by(basket_id:@basket.id,product_id:params[:product_id])
+      BasketDomain.calculate_total_prize(product_basket,params[:amount].to_i)
+    end
     redirect_to users_path
   end
 end
